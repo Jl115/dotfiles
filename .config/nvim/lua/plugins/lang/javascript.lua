0543ef8e75737b20fd70274d7262d1271ef694fb
Jl115 <joelbern006@gmail.com>
2025-06-05 21:05:44 +0200

    fix: added updates

--- .config/nvim/lua/plugins/lang/javascript.lua
+++ .config/nvim/lua/plugins/lang/javascript.lua
@@ -9,7 +9,8 @@ return {
     },
     opts = {
       servers = {
-        vtsls = {
+        tsserver = {},
+        volar = {
           filetypes = {
             "vue",
             "javascript",
@@ -19,6 +20,52 @@ return {
           },
         },
       },
+      setup = {
+        tsserver = function(_, opts)
+          require("typescript").setup({ server = opts })
+
+          local util = require("lspconfig.util")
+          opts.root_dir = function(fname)
+            local js_root = util.search_ancestors(fname, function(dir)
+              return vim.fn.filereadable(dir .. "/jsconfig.json") == 1 and dir or nil
+            end)
+            return js_root or util.root_pattern("package.json", "tsconfig.json", ".git")(fname)
+          end
+        end,
+
+        volar = function(_, opts)
+          local local_ts = vim.fn.getcwd() .. "/node_modules/typescript/lib"
+          local global_ts = vim.fn.trim(vim.fn.system("npm root -g")) .. "/typescript/lib"
+
+          local tsdk = vim.fn.isdirectory(local_ts) == 1 and local_ts
+            or (vim.fn.isdirectory(global_ts) == 1 and global_ts)
+            or (vim.fn.stdpath("data") .. "/mason/packages/typescript-language-server/node_modules/typescript/lib")
+
+          opts.init_options = {
+            typescript = { tsdk = tsdk },
+          }
+
+          opts.settings = {
+            typescript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "none",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = false,
+                includeInlayVariableTypeHints = false,
+                includeInlayPropertyDeclarationTypeHints = false,
+                includeInlayFunctionLikeReturnTypeHints = false,
+                includeInlayEnumMemberValueHints = false,
+              },
+            },
+          }
+        end,
+      },
+      on_attach = function(client, buffer)
+        if client.name == "tsserver" then
+          vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+          vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { buffer = buffer, desc = "Rename File" })
+        end
+      end,
     },
   },
 
--- .config/nvim/lua/plugins/mason/mason.lua
+++ .config/nvim/lua/plugins/mason/mason.lua
@@ -28,5 +28,5 @@ return {
       },
     },
   },
-  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+  -- { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
 }
